3d528310eee4d925d1711a34cd2e2a508329a517
reeaash <117620183+AAlimurung@users.noreply.github.com>
2023-11-29 00:26:04 +0800

    Add files via upload

---
+++ practicecozilovehtml/goo.js
@@ -0,0 +1,35 @@
+function hideprimo(){
+    const p = document.getElementById("pr");
+        if(p.style.display === "none"){
+            p.style.display = "block";
+        } else {
+            p.style.display = "none";
+        } 
+}
+
+function hidetitan(){
+    const p = document.getElementById("tt");
+        if(p.style.display === "none"){
+            p.style.display = "block";
+        } else {
+            p.style.display = "none";
+        } 
+}
+
+function hideolym(){
+    const p = document.getElementById("oly");
+        if(p.style.display === "none"){
+            p.style.display = "block";
+        } else {
+            p.style.display = "none";
+        } 
+}
+
+function hidemm(){
+    const p = document.getElementById("gg");
+        if(p.style.display === "none"){
+            p.style.display = "block";
+        } else {
+            p.style.display = "none";
+        } 
+}
